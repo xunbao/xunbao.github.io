3ec0fedb8111ebd0524d40c14663e782ee3e87bc
Sameer Taneja <direct2sameer@gmail.com>
2017-03-19 00:42:31 +0530

    js changed

--- js/home.js
+++ js/home.js
@@ -19,7 +19,7 @@ $( document ).ready(function(){
                    //alert(JSON.stringify(result));
                    $('#level').text("level "+result.level);
                    $('#que-div').html(result.description);
-                   if (result.level==12) {
+                   if (result.level==14) {
                     $('#level').text("Congrats !!")
                     $('#answer').css({"display":"none"});
                     $('#submit-button').css({"display":"none"});
