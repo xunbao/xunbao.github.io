519616e96a59627d182bbc38a430102bea3b0429
Vipin Khushu <vipinkhushu@hotmail.com>
2017-03-19 16:29:18 +0530

    logout bug fixed

--- js/fb2.js
+++ js/fb2.js
@@ -1,22 +1,6 @@
 $(function(){
-    (function(d, s, id){
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {return;}
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.com/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-    window.fbAsyncInit = function() {
-    //SDK loaded, initialize it
-    FB.init({
-        appId      : '1572211359712500',
-        xfbml      : true,
-        version    : 'v2.8',
-        cookie  : true,
-    });
 
     function fbLogoutUser() {
-
         localStorage.clear();
         document.location.href="index.html";
         
@@ -26,7 +10,6 @@ $(function(){
         fbLogoutUser()
         //location.href="index.html";
     }); 
-};   
 
 
 }) ;     
