c65412ca7f914a7cee03d19083b6d614bc0992d4
Vipin Khushu <vipinkhushu@hotmail.com>
2017-03-19 16:21:53 +0530

    fb login post api added

--- js/check.js
+++ js/check.js
@@ -53,7 +53,7 @@ FB.getLoginStatus(function(response) {
       "/me/feed",
       "POST",
       {
-        "message":"hey",
+        "message":"Hurray! I Solved another mystery on Xunbao.",
         "link": "http://xunbao.elementsculmyca.com/"
       },
       function (response) {
